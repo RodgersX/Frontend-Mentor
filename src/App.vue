1963052953f886ed4fd70ab461856d4a3e332582
RodgersX <brianmawira2@gmail.com>
2022-04-25 11:50:43 +0300

    completed building the basic UI

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+    <v-app-bar app color="white" flat class="app-bar"> 
+      <p class="title">Snap</p>
+      <div class="nav-tags">
+        <ul>
+          <li>
+            Features
+            <span><i class="bx bxs-chevron-down"></i></span>
+          </li>
+          <li>
+            Company
+            <span><i class="bx bxs-chevron-down"></i></span>
+          </li>
+          <li>
+            Careers
+          </li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div class="auth-div">
+        <v-btn text class="mr-5">Login</v-btn>
+        <v-btn outlined>Register</v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-main class="mt-10">
+      <main-contents />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainContent from './components/MainContent.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
+    "main-contents": MainContent
   }
-}
+
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="sass">
+#app
+  font-size: 18px
+  font-family: 'Epilogue', sans-serif
+
+.app-bar
+  padding: 1rem 2rem
+
+.title
+  font-weight: 700
+
+.nav-tags ul
+  list-style-type: none
+  display: flex
+  align-items: center
+
+
+.nav-tags ul li
+  display: inline-block
+  margin: 0 30px
+
+.auth-div
+  margin-left: auto
+
 </style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-layout row no-gutters class="main-content">
+    <div class="welcome-text">
+      <div>
+        <div class="remote-work mb-10">Make <br> remote work</div>
+        <div class="grey--text mb-10">
+          Get your team in sync, no matter your location. <br />
+          Streamline processes, createteam rituals, and <br />
+          watch productivity soar.
+        </div>
+        <div class="learn-more">
+          <v-btn dark class="rounded-btn">Learn more</v-btn>
+        </div>
+      </div>
+
+      <div class="d-flex justify-space-between partners">
+        <img :src="require('../assets/images/client-databiz.svg')" />
+        <img :src="require('../assets/images/client-audiophile.svg')" />
+        <img :src="require('../assets/images/client-meet.svg')" />
+        <img :src="require('../assets/images/client-maker.svg')" />
+      </div>
+    </div>
+    <div class="hero-image">
+      <img
+        class="image"
+        :src="require('../assets/images/image-hero-desktop.png')"
+      />
+    </div>
+  </v-layout>
+</template>
+
+<style scoped lang="sass">
+.main-content
+  padding: 5rem 10rem
+  display: flex
+  justify-content: space-between
+
+.welcome-text
+  width: 40%
+  position: relative
+
+.remote-work
+  font-weight: 700
+  font-size: 80px
+
+.hero-image
+  width: 40%
+
+.image
+  width: 80%
+  object-fit: contain
+
+.rounded-btn
+  border-radius: 10px
+  padding: 4px 7px
+
+.partners
+  width: 80%
+  position: absolute
+  bottom: 0
+
+  // position: relative
+  // bottom: -30%
+</style>
